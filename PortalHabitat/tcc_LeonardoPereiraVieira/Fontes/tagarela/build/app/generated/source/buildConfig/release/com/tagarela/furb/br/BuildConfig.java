2f68fb7e059baa2177a9e0cf8a13f237b49c0ed9
Dalton Solano dos Reis <dalton.reis@gmail.com>
2021-05-19 15:07:28 -0300

    Arrumando.

---
+++ PortalHabitat/tcc_LeonardoPereiraVieira/Fontes/tagarela/build/app/generated/source/buildConfig/release/com/tagarela/furb/br/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.tagarela.furb.br;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.tagarela.furb.br";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "1.0.1";
+}
